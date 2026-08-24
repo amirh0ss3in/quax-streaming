2edb416fb41be1af0df8b95bb3a3e31ded937d8b
amirh0ss3in <rezaeeamirhosein@gmail.com>
2026-08-24 09:07:26 +0000

    producer improved drastically

--- producer.py
+++ producer.py
@@ -4,45 +4,57 @@ import os
 import time
 import numpy as np
 
-DATA_DIR = '/home/ubuntu/quax_data'   # پوشه‌ای که داده‌ها اینجا دانلود شدن
+DATA_DIR = 'quax_data'   # our quax data was downloaded here.
 
 from kafka.admin import KafkaAdminClient, NewTopic
 kafka_admin = KafkaAdminClient(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
 )
 
-a_new_topic = NewTopic(name='topic_stream',   ### سه تا پارتیشن گذاشتم چون سه تا نود داریم
-                       num_partitions=3,
-                       replication_factor=1)
+print("Print available topics:", kafka_admin.list_topics())
 
-kafka_admin.create_topics(new_topics=[a_new_topic])
+if 'topic_stream' not in kafka_admin.list_topics():
+    a_new_topic = NewTopic(name='topic_stream',
+                        num_partitions=6, # 3 VMs, 2x Core per VM, So we need *at least* 6 partitions.
+                        replication_factor=1)
+    kafka_admin.create_topics(new_topics=[a_new_topic])
 
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
 
-while 1:
+data_i = np.empty((31, 4096, 2048), dtype=np.float32)
+data_q = np.empty((31, 4096, 2048), dtype=np.float32)
+
+for file_index in range(31):
+    data_i[file_index] = np.fromfile(
+        f'{DATA_DIR}/duck_i_{file_index:05d}.dat',
+        dtype='<f4'
+    ).reshape(4096, 2048)
+
+    data_q[file_index] = np.fromfile(
+        f'{DATA_DIR}/duck_q_{file_index:05d}.dat',
+        dtype='<f4'
+    ).reshape(4096, 2048)
+
+print(data_i.shape, data_q.shape)
+
+while True:
     for file_index in range(31):
-        idata = f'duck_i_{file_index:05d}.dat'
-        qdata = f'duck_q_{file_index:05d}.dat'
-
-        with open(f'{DATA_DIR}/{idata}', 'rb') as f: # فایل از روی مستر خوانده میشود
-            raw_bytes = f.read()
-        flat = np.frombuffer(raw_bytes, dtype='<f4')
-        i_table = flat.reshape(4096, 2048)
-
-        # همون کار برای کیو
-        with open(f'{DATA_DIR}/{qdata}', 'rb') as f:
-            raw_bytes_q = f.read()
-        flat_q = np.frombuffer(raw_bytes_q, dtype='<f4')
-        q_table = flat_q.reshape(4096, 2048)
+        i_table = data_i[file_index]
+        q_table = data_q[file_index]
 
         for scan_index in range(4096):
             iscan = i_table[scan_index]
             qscan = q_table[scan_index]
+
             scan_id = f'{file_index}_{scan_index}'
             value = iscan.tobytes() + qscan.tobytes()
-            producer.send(topic='topic_stream',
-                          key=scan_id.encode(),
-                          value=value)
+
+            producer.send(
+                topic='topic_stream',
+                key=scan_id.encode(),
+                value=value
+            )
+
         producer.flush()
         time.sleep(4)
